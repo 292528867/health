9eec0ca63a371be5b5db835bfe3f295a34918c5e
mars <cosmo_mars@outlook.com>
2015-07-02 18:27:31 +0800

    修改手机验证正则表达式

--- src/main/java/com/wonders/xlab/healthcloud/utils/ValidateUtils.java
+++ src/main/java/com/wonders/xlab/healthcloud/utils/ValidateUtils.java
@@ -9,6 +9,6 @@ public class ValidateUtils {
 
     /** 判断是否为合法的手机号码 */
     public static boolean validateTel(String tel) {
-        return isNotEmpty(tel) && tel.matches("/^1((3|5|8){1}\\d{1}|70|77)\\d{8}$/");
+        return isNotEmpty(tel) && tel.matches("^1((3|5|8){1}\\d{1}|70|77)\\d{8}$");
     }
 }
