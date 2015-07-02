f41dddbc1db0821454b338e0ee0083e5af2d7390
万华秋 <303701750@qq.com>
2015-07-02 10:51:52 +0800

    test add a file

--- src/main/java/com/wonders/xlab/healthcloud/entity/User.java
+++ src/main/java/com/wonders/xlab/healthcloud/entity/User.java
@@ -2,4 +2,23 @@ package com.wonders.xlab.healthcloud.entity;
 
 public class User {
 
+    private  String name;
+
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
