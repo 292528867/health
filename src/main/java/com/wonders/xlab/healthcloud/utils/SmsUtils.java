bf07723c643465f6ea038569323527ad5c0d4cc1
李玄武 <410230697@qq.com>
2015-07-02 21:11:51 +0800

    update

--- src/main/java/com/wonders/xlab/healthcloud/utils/SmsUtils.java
+++ src/main/java/com/wonders/xlab/healthcloud/utils/SmsUtils.java
@@ -1,6 +1,7 @@
 package com.wonders.xlab.healthcloud.utils;
 
 import com.bcloud.msg.http.HttpSender;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by wangqiang on 15/7/2.
@@ -10,19 +11,18 @@ public class SmsUtils {
     public static final String SMS_SRV_URL = "http://222.73.117.158/msg/";
     public static final String SMS_ACT_USER = "jiekou-clcs-01";
     public static final String SMS_ACT_PWD = "Tch498965";
-    public static String SMS_VALID_CODE_CONTENT = "亲爱的用户，您的验证码是%s，5分钟内有效。";
+    public static final String SMS_VALID_CODE_CONTENT = "亲爱的用户，您的验证码是%s，5分钟内有效。";
 
     private SmsUtils() {
 
     }
 
-    public static int sendValidCode(String mobiles,String validCode) {
+    public static int sendValidCode(String mobiles, String validCode) {
         try {
-
-            SMS_VALID_CODE_CONTENT = String.format(SMS_VALID_CODE_CONTENT,validCode);
-            String resultCode = HttpSender.batchSend(SMS_SRV_URL, SMS_ACT_USER, SMS_ACT_PWD, mobiles, SMS_VALID_CODE_CONTENT, true, null, null);
-            String status = resultCode.substring(resultCode.indexOf(",")+1,resultCode.indexOf(",")+2);
-            if (status.equals("0")){
+            String content = String.format(SMS_VALID_CODE_CONTENT, validCode);
+            String resultCode = HttpSender.batchSend(SMS_SRV_URL, SMS_ACT_USER, SMS_ACT_PWD, mobiles, content, true, null, null);
+            String status = resultCode.substring(resultCode.indexOf(",") + 1, resultCode.indexOf(",") + 2);
+            if (StringUtils.equals(status, "0")) {
                 return 0;
             }
             return 1;
@@ -33,7 +33,7 @@ public class SmsUtils {
 
     public static void main(String[] args) {
         String mobiles = "15800691693,13621673989";//手机号码，多个号码使用","分割
-        int resultCode = sendValidCode(mobiles,"1234");
+        int resultCode = sendValidCode(mobiles, "1234");
         System.out.println("resultCode = " + resultCode);
     }
 
