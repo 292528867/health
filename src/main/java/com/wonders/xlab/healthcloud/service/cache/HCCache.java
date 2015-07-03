127a60399fb47c95c2569f885d42c4de60215885
徐烜 <bioxuxuan@gmail.com>
2015-07-03 10:32:10 +0800

    ehcache缓存更新！

---
+++ src/main/java/com/wonders/xlab/healthcloud/service/cache/HCCache.java
@@ -0,0 +1,30 @@
+package com.wonders.xlab.healthcloud.service.cache;
+
+import java.util.Map;
+
+/**
+ * 缓存。
+ * @author xu
+ *
+ * @param <KEYTYPE> 缓存key类型
+ * @param <VALUETYPE> 缓存value类型
+ */
+public interface HCCache<KEYTYPE, VALUETYPE> {
+	/**
+	 * 添加一个element到缓存。
+	 */
+	public void addToCache(KEYTYPE key, VALUETYPE value);
+	/**
+	 * 添加一组element到缓存。
+	 */
+	public void addToCache(Map<KEYTYPE, VALUETYPE> map);
+	/**
+	 * 从缓存中删除一个element。
+	 */
+	public void removeFromCache(KEYTYPE key);
+	/**
+	 * 从缓存中获取一个element。
+	 * @return 没有找到返回null。
+	 */
+	public VALUETYPE getFromCache(KEYTYPE key);
+}
---
+++ src/main/java/com/wonders/xlab/healthcloud/service/cache/HCCacheProxy.java
@@ -0,0 +1,49 @@
+package com.wonders.xlab.healthcloud.service.cache;
+
+import java.util.Map;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
+
+/**
+ * 缓存代理类，用于代理具体的缓存实现，方便以后切换缓存实现。
+ * @author xu
+ *
+ * @param <KEYTYPE>
+ * @param <VALUETYPE>
+ */
+public class HCCacheProxy<KEYTYPE, VALUETYPE> implements HCCache<KEYTYPE, VALUETYPE>{
+	/** ehCache缓存实现 */
+	private Cache ehCache;
+	public HCCacheProxy(Cache cache) {
+		this.ehCache = cache;
+	}
+	
+	@Override
+	public void addToCache(KEYTYPE key, VALUETYPE value) {
+		Element element =  new Element(key, value);
+		this.ehCache.put(element);
+	}
+
+	@Override
+	public void addToCache(Map<KEYTYPE, VALUETYPE> map) {
+		for (KEYTYPE key : map.keySet()) 
+			this.addToCache(key, map.get(key));
+	}
+
+	@Override
+	public void removeFromCache(KEYTYPE key) {
+		this.ehCache.remove(key);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public VALUETYPE getFromCache(KEYTYPE key) {
+		Element element = this.ehCache.get(key);
+		if (element != null) 
+			return (VALUETYPE) element.getObjectValue();
+		else
+			return null;
+	}
+	
+}
