b10091453d619e453c3a9a785352fd8f24218f65
徐烜 <bioxuxuan@gmail.com>
2015-07-04 19:16:22 +0800

    app端6个页面显示功能！

--- src/main/java/com/wonders/xlab/healthcloud/controller/discovery/AppController.java
+++ src/main/java/com/wonders/xlab/healthcloud/controller/discovery/AppController.java
@@ -1,5 +1,9 @@
 package com.wonders.xlab.healthcloud.controller.discovery;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wonders.xlab.healthcloud.dto.discovery.HealthInfoDto;
 import com.wonders.xlab.healthcloud.dto.result.ControllerResult;
 import com.wonders.xlab.healthcloud.entity.customer.User;
+import com.wonders.xlab.healthcloud.entity.discovery.HealthInfo;
 import com.wonders.xlab.healthcloud.repository.customer.UserRepository;
+import com.wonders.xlab.healthcloud.repository.discovery.HealthCategoryRepository;
+import com.wonders.xlab.healthcloud.repository.discovery.HealthInfoRepository;
 import com.wonders.xlab.healthcloud.service.drools.DiscoveryRuleService;
 
 /**
@@ -27,6 +35,8 @@ public class AppController {
 	private DiscoveryRuleService discoveryRuleService;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private HealthInfoRepository healthInfoRepository;
 	
 	// 首页显示推荐健康info信息
 	@RequestMapping(value = "recommand/articles/{userId}")
@@ -37,8 +47,11 @@ public class AppController {
 			return new ControllerResult<String>().setRet_code(-1).setRet_values("用户不存在").setMessage("用户不存在！");
 		Long[] ids = discoveryRuleService.pushArticles(user);
 		
-		// TODO：
-		return new ControllerResult<Long[]>().setRet_code(0).setRet_values(ids).setMessage("成功！");
+		List<HealthInfo> healthInfos = this.healthInfoRepository.findAll(Arrays.asList(ids));
+		List<HealthInfoDto> healthInfoDtoes = new ArrayList();
+		for (HealthInfo h : healthInfos)
+			healthInfoDtoes.add(new HealthInfoDto().toNewHealthInfoDto(h));
+		return new ControllerResult<List<HealthInfoDto>>().setRet_code(0).setRet_values(healthInfoDtoes).setMessage("成功");
 	}
 	
 	// 显示更多信息，分类标签信息返回
