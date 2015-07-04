ae72892dd84dc5eca01530c416d91baf01169ddf
李玄武 <410230697@qq.com>
2015-07-04 20:11:02 +0800

    up

--- src/main/java/com/wonders/xlab/healthcloud/controller/EmController.java
+++ src/main/java/com/wonders/xlab/healthcloud/controller/EmController.java
@@ -9,15 +9,11 @@ import com.wonders.xlab.healthcloud.utils.EMUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by lixuanwu on 15/7/4.
@@ -53,19 +49,15 @@ public class EmController extends AbstractBaseController<EmMessages, Long> {
     @RequestMapping(value = "sendmessage", method = RequestMethod.POST)
     public ControllerResult sendmessage(@RequestBody String body) {
 
-        List<MediaType> mediaTypes = new ArrayList<MediaType>() {{
-            add(MediaType.APPLICATION_JSON);
-        }};
         HttpHeaders header = new HttpHeaders();
-        //添加请求头
-        header.setAccept(mediaTypes);
+
         //配接获取环信token请求体
         String dd = "{\"target_type\": \"users\",\n" +
                 "    \"target\": [\"lixuanwu\", \"qiuqiu\", \"chaochao\"], \n" +
                 "    \"msg\": {\n" +
                 "        \"type\":\"txt\",\n" +
                 "        \"msg\":\"hello from wanhua\"},\"from\":\"lixuanwu\"}";
-        ResponseEntity<String> responseEntity = (ResponseEntity<String>) emUtils.requstEMChart(header, HttpMethod.POST, dd, "messages", String.class);
+        ResponseEntity<String> responseEntity = (ResponseEntity<String>) emUtils.requstEMChart(HttpMethod.POST, dd, "messages", String.class);
 
         return new ControllerResult().setRet_code(0).setRet_values(responseEntity).setMessage("");
 
