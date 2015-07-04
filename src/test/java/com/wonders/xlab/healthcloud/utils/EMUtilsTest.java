abc04b7ea4d3ed76870a72aaf18102b1d6a47c56
万华秋 <303701750@qq.com>
2015-07-04 17:44:26 +0800

    添加测试

---
+++ src/test/java/com/wonders/xlab/healthcloud/utils/EMUtilsTest.java
@@ -0,0 +1,14 @@
+package com.wonders.xlab.healthcloud.utils;
+
+
+import org.junit.Test;
+
+/**
+ * Created by Jeffrey on 15/7/4.
+ */
+public class EMUtilsTest {
+
+    @Test
+    public void testRequstEMChart() throws Exception {
+    }
+}
