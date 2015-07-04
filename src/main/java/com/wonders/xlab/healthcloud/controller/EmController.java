5b999392df60d11ef46bfc2c29f26d6429f2110c
李玄武 <410230697@qq.com>
2015-07-04 18:45:24 +0800

    update

---
+++ src/main/java/com/wonders/xlab/healthcloud/controller/EmController.java
@@ -0,0 +1,62 @@
+package com.wonders.xlab.healthcloud.controller;
+
+import com.wonders.xlab.healthcloud.dto.result.ControllerResult;
+import com.wonders.xlab.healthcloud.utils.EMUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lixuanwu on 15/7/4.
+ */
+@RestController
+@RequestMapping(value = "em")
+public class EmController {
+
+    @Autowired
+    private EMUtils emUtils;
+
+
+    /**
+     * 发送文本消息
+     *
+     * @param body
+     * @return
+     */
+    @RequestMapping(value = "sendmessage" ,method = RequestMethod.POST)
+    public ControllerResult sendmessage(@RequestBody String body){
+
+        List<MediaType> mediaTypes = new ArrayList<MediaType>() {{
+            add(MediaType.APPLICATION_JSON);
+        }};
+        HttpHeaders header = new HttpHeaders();
+        //添加请求头
+        header.setAccept(mediaTypes);
+        //配接获取环信token请求体
+        body = "{\n" +
+                "    \"target_type\" : \"users\",\n" +
+                "    \"target\" : [\"lixuanwu\", \"qiuqiu\", \"chaochao\"], \n" +
+                "    \"msg\" : {\n" +
+                "        \"type\" : \"txt\",\n" +
+                "        \"msg\" : \"hello from rest\" \n" +
+                "        },\n" +
+                "    \"from\" : \"lixuanwu\"\n" +
+                "}";
+        ResponseEntity <String> responseEntity = (ResponseEntity<String>) emUtils.requstEMChart(header, HttpMethod.POST, body, "messages", String.class);
+
+        return new ControllerResult().setRet_code(0).setRet_values(responseEntity).setMessage("");
+
+    }
+
+
+
+}
--- src/main/java/com/wonders/xlab/healthcloud/utils/EMUtils.java
+++ src/main/java/com/wonders/xlab/healthcloud/utils/EMUtils.java
@@ -1,6 +1,5 @@
 package com.wonders.xlab.healthcloud.utils;
 
-import com.wonders.xlab.healthcloud.ApiProperties;
 import com.wonders.xlab.healthcloud.dto.EMToken;
 import com.wonders.xlab.healthcloud.service.cache.HCCache;
 import com.wonders.xlab.healthcloud.service.cache.HCCacheProxy;
@@ -8,6 +7,8 @@ import net.sf.ehcache.Cache;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
@@ -23,12 +24,10 @@ import java.util.Map;
  * Created by Jeffrey on 15/7/4.
  */
 @Component
+@PropertySource("classpath:emcart.api/emchartApi.properties")
 public class EMUtils {
 
     private RestTemplate restTemplate = new RestTemplate();
-
-    @Autowired
-    private ApiProperties apiProperties;
 
     @Autowired
     @Qualifier(value = "emCache")
@@ -41,6 +40,20 @@ public class EMUtils {
         hcCache = new HCCacheProxy<>(emCache);
     }
 
+    public static final String url = "http://a1.easemob.com/xlab/ugyufuy/{key}";
+
+    @Value("${API_SERVER_HOST}")
+    public String API_SERVER_HOST;
+
+    @Value("${APPKEY}")
+    public String APPKEY;
+
+    @Value("${APP_CLIENT_ID}")
+    public String APP_CLIENT_ID;
+
+    @Value("${APP_CLIENT_SECRET}")
+    public String APP_CLIENT_SECRET;
+
     public void pushTokenToCache() {
 
         List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>() {{
@@ -51,9 +64,9 @@ public class EMUtils {
         header.setAccept(acceptableMediaTypes);
         //配接获取环信token请求体
         String body = "{\"grant_type\":\"client_credentials\",\"client_id\":\"" +
-                apiProperties.APP_CLIENT_ID +
+                APP_CLIENT_ID +
                 "\",\"client_secret\":\"" +
-                apiProperties.APP_CLIENT_SECRET + "\"}";
+                APP_CLIENT_SECRET + "\"}";
 
         ResponseEntity result = requstEMChart(header, HttpMethod.POST, body, "token", EMToken.class);
 
@@ -76,7 +89,7 @@ public class EMUtils {
         Map<String, Object> uriVariables = new HashMap<>();
         uriVariables.put("key", path);
         ResponseEntity<?> result = restTemplate.exchange(
-                ApiProperties.url,
+                url,
                 method,
                 entity,
                 classz,
@@ -88,6 +101,35 @@ public class EMUtils {
     }
 
 
+    public String getAPI_SERVER_HOST() {
+        return API_SERVER_HOST;
+    }
 
+    public void setAPI_SERVER_HOST(String API_SERVER_HOST) {
+        this.API_SERVER_HOST = API_SERVER_HOST;
+    }
 
+    public String getAPPKEY() {
+        return APPKEY;
+    }
+
+    public void setAPPKEY(String APPKEY) {
+        this.APPKEY = APPKEY;
+    }
+
+    public String getAPP_CLIENT_ID() {
+        return APP_CLIENT_ID;
+    }
+
+    public void setAPP_CLIENT_ID(String APP_CLIENT_ID) {
+        this.APP_CLIENT_ID = APP_CLIENT_ID;
+    }
+
+    public String getAPP_CLIENT_SECRET() {
+        return APP_CLIENT_SECRET;
+    }
+
+    public void setAPP_CLIENT_SECRET(String APP_CLIENT_SECRET) {
+        this.APP_CLIENT_SECRET = APP_CLIENT_SECRET;
+    }
 }
