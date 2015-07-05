3e061b552a8b00b96090a5847bda7b93163bcc9c
万华秋 <303701750@qq.com>
2015-07-05 19:48:32 +0800

    update

--- src/test/java/com/wonders/xlab/healthcloud/utils/EMUtilsTest.java
+++ src/test/java/com/wonders/xlab/healthcloud/utils/EMUtilsTest.java
@@ -1,13 +1,17 @@
 package com.wonders.xlab.healthcloud.utils;
 
 
-import org.apache.commons.lang3.StringUtils;
+import com.wonders.xlab.framework.Application;
 import org.junit.Test;
-import org.springframework.http.*;
-import org.springframework.util.Assert;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -18,9 +22,16 @@ import java.util.Map;
 /**
  * Created by Jeffrey on 15/7/4.
  */
+@RunWith(SpringJUnit4ClassRunner.class)   // 1
+@SpringApplicationConfiguration(classes = Application.class)   // 2
+@WebAppConfiguration   // 3
 public class EMUtilsTest {
 
-    private RestTemplate restTemplate = new RestTemplate();
+//    private RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    private EMUtils emUtils;
+
     private static final String Authorization = "Authorization:Bearer YWMt0Ujc-CGAEeWGzdFWKNW-6QAAAU-I7R8lpklXS8QMECBLBFGdAGxHXWcckj";
 
     @Test
@@ -53,51 +64,13 @@ public class EMUtilsTest {
         Map<String, Object> request = new HashMap<String, Object>(){{
             put("file", file);
         }};
-        ResponseEntity result = this.requstEMChart(headers, HttpMethod.POST, request, "chatfiles", String.class);
+        ResponseEntity result = emUtils.requestEMChart(headers, HttpMethod.POST, request, "chatfiles", String.class);
         System.out.println("result.getBody() = " + result.getBody());
     }
 
     @Test
     public void testRequstEMChatGet() throws Exception {
-        List<MediaType> mediaTypes = new ArrayList<MediaType>(){{
-            add(MediaType.APPLICATION_JSON);
-        }};
-        HttpHeaders headers = new HttpHeaders();
-//        headers.setAccept(mediaTypes);
-        headers.add("Authorization","Bearer YWMtLTlNuCI6EeWD--V4NuV_CgAAAU-Nq-Qskj9BKL_nnRqWuGLEIW8lACwLMp4");
-
-        ResponseEntity result = this.requstEMChart(headers, HttpMethod.GET, null, "chatgroups", String.class);
+        ResponseEntity result = emUtils.requestEMChart(HttpMethod.GET, null, "chatgroups", String.class);
         System.out.println("result.getBody() = " + result.getBody());
-    }
-
-    public ResponseEntity<?> requstEMChart(HttpHeaders headers, HttpMethod method, Object body, String path, Class<?> classz) {
-
-        if (headers == null) {
-            headers = new HttpHeaders();
-        }
-        Map<String, Object> requestBody = new HashMap<>();
-        MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
-        param.setAll((Map<String, Object>) body);
-
-        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(param,headers);
-//        if (StringUtils.isEmpty(body)) {
-//            entity = new HttpEntity<>(headers);
-//        } else {
-//            entity = new HttpEntity<>(body, headers);
-//        }
-
-        Map<String, Object> uriVariables = new HashMap<>();
-        uriVariables.put("key", path);
-        ResponseEntity<?> result = restTemplate.exchange(
-//                "http://10.1.64.179:8080/xlab-healthcloud/user/{key}",
-                "http://a1.easemob.com/xlab/ugyufuy/{key}",
-                method,
-                entity,
-                classz,
-                uriVariables
-        );
-
-        Assert.isTrue(HttpStatus.OK.equals(result.getStatusCode()));
-        return result;
     }
 }
