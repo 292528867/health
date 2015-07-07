a471c06720e05913c4f82a12ce42621f2c865503
徐烜 <bioxuxuan@gmail.com>
2015-07-07 11:50:15 +0800

    规则更新！

--- src/main/java/com/wonders/xlab/healthcloud/service/drools/MyDroolsApplication.java
+++ src/main/java/com/wonders/xlab/healthcloud/service/drools/MyDroolsApplication.java
@@ -3,7 +3,8 @@ package com.wonders.xlab.healthcloud.service.drools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.wonders.xlab.healthcloud.service.drools.discovery.article.DiscoveryRuleService;
+import com.wonders.xlab.healthcloud.service.cache.EhCacheConfiguration;
+import com.wonders.xlab.healthcloud.service.drools.discovery.article.DiscoveryArticleRuleService;
 
 /**
  * 测试MyDroolsConfiguration。
@@ -12,12 +13,13 @@ import com.wonders.xlab.healthcloud.service.drools.discovery.article.DiscoveryRu
  */
 public class MyDroolsApplication {
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(MyDroolsConfiguration.class, DiscoveryRuleService.class);
+		SpringApplication app = new SpringApplication(MyDroolsConfiguration.class, DiscoveryArticleRuleService.class);
 		app.setWebEnvironment(false);
 		ApplicationContext ctx = app.run(args);
 		
-		DiscoveryRuleService discoveryRuleService = ctx.getBean(DiscoveryRuleService.class);
-		discoveryRuleService.testRule();
+		DiscoveryArticleRuleService discoveryArticleRuleService = ctx.getBean(DiscoveryArticleRuleService.class);
+//		discoveryRuleService.testRule();
+		discoveryArticleRuleService.testRule();
 		
 //		// 获取feed规则服务执行
 //		FeedRuleService feedRuleService = ctx.getBean(FeedRuleService.class);
