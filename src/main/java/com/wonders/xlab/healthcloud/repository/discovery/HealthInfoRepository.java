e18ce4083cb4f52846e49215cdf4c78d5b4ef96a
徐烜 <bioxuxuan@gmail.com>
2015-07-07 19:51:49 +0800

    文章推送规则更新！

--- src/main/java/com/wonders/xlab/healthcloud/repository/discovery/HealthInfoRepository.java
+++ src/main/java/com/wonders/xlab/healthcloud/repository/discovery/HealthInfoRepository.java
@@ -9,5 +9,7 @@ import com.wonders.xlab.healthcloud.entity.discovery.HealthInfo;
 
 public interface HealthInfoRepository extends MyRepository<HealthInfo, Long> {
 	List<HealthInfo> findByHealthCategoryId(Long id);
+	@Query("select info from HealthInfo info where info.healthCategory.id in (?1)")
+	List<HealthInfo> findHealthCategoryIds(Long... ids);
 	HealthInfo findByHealthCategoryIdAndId(Long caid, Long infoid);
 }
