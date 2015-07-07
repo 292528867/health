d409d2164d03a8b4c0046857741a8c6c24c95801
wukai <wukai@wondersgroup.com>
2015-07-07 21:04:09 +0800

    轻问诊，发送问题至第三方app，并获取回答

--- src/main/java/com/wonders/xlab/healthcloud/controller/ThirdAppController.java
+++ src/main/java/com/wonders/xlab/healthcloud/controller/ThirdAppController.java
@@ -1,5 +1,6 @@
 package com.wonders.xlab.healthcloud.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wonders.xlab.healthcloud.dto.QuestionRequestBody;
 import com.wonders.xlab.healthcloud.dto.ThirdAppAccount;
 import com.wonders.xlab.healthcloud.dto.result.ControllerResult;
@@ -8,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,70 +47,78 @@ public class ThirdAppController {
      * @throws Exception
      */
     public String askQuestions(String appName, QuestionRequestBody requestBody) throws Exception{
-
+        String resultString = "";
         ThirdAppAccount appAccount = ThirdAppConnUtils.appDatas.get(appName);
         switch (appName){
             case "basksugar":{
-                    //血糖高管
-                    HttpHeaders headers = new HttpHeaders();
-                    List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>() {{
-                        add(MediaType.TEXT_PLAIN);
-                    }};
-                    headers.setAccept(acceptableMediaTypes);
-                    Map<String, String> requestMap = new HashMap<>();
-                    //问题内容
-                    requestMap.put("freeProductInfo.topic", requestBody.getQuestion());
-                    //clientId 登录获取
-                    requestMap.put("freeProductInfo.clientId", appAccount.getClientId());
-                    requestMap.put("freeProductInfo.clientName", "");
-                    requestMap.put("freeProductInfo.pictureUrl", "");
-                    //sign
-                    requestMap.put("sign", UUID.randomUUID().toString().replaceAll("-", ""));
+                //血糖高管
+                if(StringUtils.isEmpty(appAccount.getClientId())){
+                    ThirdAppConnUtils.loginApp(appName);
+                }
+                HttpHeaders headers = new HttpHeaders();
+                List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>() {{
+                    add(MediaType.TEXT_PLAIN);
+                }};
+                headers.setAccept(acceptableMediaTypes);
+                Map<String, String> requestMap = new HashMap<>();
+                //问题内容
+                requestMap.put("freeProductInfo.topic", requestBody.getQuestion());
+                //clientId 登录获取
+                requestMap.put("freeProductInfo.clientId", appAccount.getClientId());
+                requestMap.put("freeProductInfo.clientName", "");
+                requestMap.put("freeProductInfo.pictureUrl", "");
+                //sign
+                requestMap.put("sign", UUID.randomUUID().toString().replaceAll("-", ""));
 
-                    Map<String, Object> uriVariables = new HashMap<>();
-                    uriVariables.put("m1", appAccount.getChannel());
+                Map<String, Object> uriVariables = new HashMap<>();
+                uriVariables.put("m1", appAccount.getChannel());
 
-                    ThirdAppConnUtils.requestEMChart(headers, HttpMethod.POST, requestMap, String.class, appAccount.getAskUrl(), uriVariables);
-                    break;
+                ResponseEntity responseEntity = ThirdAppConnUtils.requestEMChart(headers, HttpMethod.POST, requestMap, String.class, appAccount.getAskUrl(), uriVariables);
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map<String, String> resultMap = objectMapper.readValue((String) responseEntity.getBody(), HashMap.class);
+                resultString = resultMap.get("code");
+                break;
                 }
             case "ememed": {
                 //薏米医生
-                if(StringUtils.isBlank(appAccount.getLoginToken())){
+                if(StringUtils.isEmpty(appAccount.getLoginToken())){
                     ThirdAppConnUtils.loginApp(appName);
                 }
                 List<String> doctorList = ThirdAppConnUtils.findDoctorList(appAccount);
+                long cursor = System.currentTimeMillis()%doctorList.size();
                 //随即取一个医生发送
-                Random random = new Random(doctorList.size());
-                String doctorId = doctorList.get(random.nextInt());
-//                for(String doctorId : doctorList){
-                    HttpHeaders headers = new HttpHeaders();
-                    List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>() {{
-                        add(MediaType.APPLICATION_FORM_URLENCODED);
-                    }};
-                    headers.setAccept(acceptableMediaTypes);
-                    Map<String, String> requestMap = new HashMap<>();
-                    requestMap.put("cotent", requestBody.getQuestion());
-                    requestMap.put("upload_file", "");
-                    requestMap.put("free", "1");
-                    requestMap.put("price", "0.0");
-                    requestMap.put("doctorid", doctorId);
-                    requestMap.put("memberid", appAccount.getClientId());
-                    requestMap.put("money_null", "1");
-                    requestMap.put("token", appAccount.getLoginToken());
-                    requestMap.put("appversion", appAccount.getAppVersion());
-                    requestMap.put("ordertype", "1");
-                    requestMap.put("channel", appAccount.getChannel());
+                String doctorId = doctorList.get((int)cursor);
+                HttpHeaders headers = new HttpHeaders();
+                List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>() {{
+                    add(MediaType.APPLICATION_FORM_URLENCODED);
+                }};
+                headers.setAccept(acceptableMediaTypes);
+                Map<String, String> requestMap = new HashMap<>();
+                requestMap.put("content", requestBody.getQuestion());
+                requestMap.put("upload_file", "");
+                requestMap.put("free", "1");
+                requestMap.put("price", "0.0");
+                requestMap.put("doctorid", doctorId);
+                requestMap.put("memberid", appAccount.getClientId());
+                requestMap.put("money_null", "1");
+                requestMap.put("token", appAccount.getLoginToken());
+                requestMap.put("appversion", appAccount.getAppVersion());
+                requestMap.put("ordertype", "1");
+                requestMap.put("channel", appAccount.getChannel());
 
-                    ThirdAppConnUtils.requestEMChart(headers, HttpMethod.POST, requestMap, String.class, appAccount.getAskUrl(), null);
+                ResponseEntity responseEntity = ThirdAppConnUtils.requestEMChart(headers, HttpMethod.POST, requestMap, String.class, appAccount.getAskUrl(), null);
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map<String, String> resultMap = objectMapper.readValue((String) responseEntity.getBody(), HashMap.class);
+                System.out.println("resultMap = " + resultMap);
+                resultString = resultMap.get("success");
 
-//                }
                 break;
             }
 
             default : break;
         }
 
-        return "";
+        return resultString;
     }
 
 
