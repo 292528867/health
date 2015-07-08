72cbb3f5cb1b86f7e639784232f1b25575d95f94
mars <cosmo_mars@outlook.com>
2015-07-08 11:50:14 +0800

    修改健康包

--- src/main/java/com/wonders/xlab/healthcloud/repository/hcpackage/HcPackageRepository.java
+++ src/main/java/com/wonders/xlab/healthcloud/repository/hcpackage/HcPackageRepository.java
@@ -2,9 +2,15 @@ package com.wonders.xlab.healthcloud.repository.hcpackage;
 
 import com.wonders.xlab.framework.repository.MyRepository;
 import com.wonders.xlab.healthcloud.entity.hcpackage.HcPackage;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by mars on 15/7/3.
  */
 public interface HcPackageRepository extends MyRepository<HcPackage, Long> {
+
+    @Query("from HcPackage p order by p.createdDate desc")
+    List<HcPackage> findAllOrderByCreateDate();
 }
