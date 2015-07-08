542eaacc223f0d904aca08ceaf10af441900dcec
万华秋 <303701750@qq.com>
2015-07-08 18:05:23 +0800

    加入计划

--- src/main/java/com/wonders/xlab/healthcloud/controller/hcpackage/UserPackageOrderController.java
+++ src/main/java/com/wonders/xlab/healthcloud/controller/hcpackage/UserPackageOrderController.java
@@ -13,10 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Jeffrey on 15/7/8.
  */
+@RestController
+@RequestMapping("plan")
 public class UserPackageOrderController extends AbstractBaseController<UserPackageOrder, Long> {
 
     @Autowired
