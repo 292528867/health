2beac1de0ab274f138bfff3be51f1d8be4c780b3
= <yukui@wondersgroup.com>
2015-07-12 17:30:48 +0800

    轻问诊接口修改

--- src/main/java/com/wonders/xlab/healthcloud/utils/Constant.java
+++ src/main/java/com/wonders/xlab/healthcloud/utils/Constant.java
@@ -6,23 +6,23 @@ package com.wonders.xlab.healthcloud.utils;
 public class Constant {
 
     //ios信鸽access_id
-    private final static long XINGE_IOS_ACCESS_ID = 2200129154L;
+    public static final long XINGE_IOS_ACCESS_ID = 2200129154L;
     //ios信鸽secret_key
-    private final static String XINGE_IOS_SECRET_KEY = "81da0383dce9446fefb61fe4d6756592";
+    public static final String XINGE_IOS_SECRET_KEY = "81da0383dce9446fefb61fe4d6756592";
     //android信鸽access_id
-    private final static long XINGE_ANDROID_ACCESS_ID = 2100129201;
+    public static final long XINGE_ANDROID_ACCESS_ID = 2100129201;
     //android信鸽secret_key
-    private final static String XINGE_ANDROID_SECRET_KEY = "2b6434d49089b3fe2db61cf3d0e04708";
+    public static final String XINGE_ANDROID_SECRET_KEY = "2b6434d49089b3fe2db61cf3d0e04708";
 
     //轻问诊回复推送描述
-    private final static String INTERROGATION_REPLY_PUSH = "";
+    public static final String INTERROGATION_REPLY_PUSH = "特聘专家%d耗时%s分钟，认真看完了你的问题并作出专业解答。快点查阅看看吧。";
     //轻问诊的医生数量的问候语
-    private final static String INTERROGATION_GRETTINGS = "你并没有发烧但感到头疼脑热？你并没有心脏病，但胸闷气短？你有一些小症状但不知是什么情况？不用出门，轻问诊%s位专家帮你了解自己的身体现状。";
+    public static final String INTERROGATION_GRETTINGS = "你并没有发烧但感到头疼脑热？你并没有心脏病，但胸闷气短？你有一些小症状但不知是什么情况？不用出门，轻问诊%s位专家帮你了解自己的身体现状。";
     //轻问诊的提示例子
-    private final static String INTERROGATION_questionSample = "我40岁，糖尿病7年，血糖一直偏高，空腹血糖一直在9左右，餐后血糖13左右。一直在吃阿卡波糖片，" +
+    public static final String INTERROGATION_QUESTION_SAMPLE = "我40岁，糖尿病7年，血糖一直偏高，空腹血糖一直在9左右，餐后血糖13左右。一直在吃阿卡波糖片，" +
             "前段时间换了药，不但血糖没有降低，反而出现了心慌、胸闷、气短的症状。现在不知道要怎么办，需要打胰岛素吗？";
     //轻问诊的超时描述
-    private final static String INTERROGATION_OVERTIME_CONTENT = "此刻我们已从专业团队中预约了最符合您情况的专家为您解答困惑，为了您的健康，给专家留一小会时间吧~";
+    public static final String INTERROGATION_OVERTIME_CONTENT = "此刻我们十分理解您的担忧与焦虑，我们已布下天罗地网缉拿专家为您解答困惑。稍后专家将亲自奉上本月全勤奖金XX健康豆，别客气，拿着！";
     //轻问诊的等待描述
-    private final static String INTERROGATION_WAIT_CONTENT = "此刻我们已从专业团队中预约了最符合您情况的专家为您解答困惑，为了您的健康，给专家留一小会时间吧~";
+    public static final String INTERROGATION_WAIT_CONTENT = "此刻我们已从专业团队中预约了最符合您情况的专家为您解答困惑，为了您的健康，给专家留一小会时间吧~";
 }
