a5bca10f68e318452a4cbad30ebe0c4a77974402
wukai <wukai@wondersgroup.com>
2015-07-12 10:34:36 +0800

    计划包编辑&新增页面修改

--- src/main/webapp/console/js/loadAllPackages.js
+++ src/main/webapp/console/js/loadAllPackages.js
@@ -14,17 +14,18 @@ $.get(typeUrl, function (data) {
         datas += "<option value='" + value.id + "'>" + value.title + "</option>";
 
     });
-    //$("#type").append(datas);
     $(".type-select").append(datas);
 });
 
-if(localStorage.getItem('typeId')!= null) {
-    //searchPackage(localStorage.getItem('typeId'));
-}
+/*if(localStorage.getItem('typeId')!= null) {
+    searchPackage(localStorage.getItem('typeId'));
+}*/
 
 //打开编辑页面
 function changePackage(id) {
     $.get(onePackageUrl+id, function (data) {
+        console.log(data);
+        $('#change-id').val(id);
         $('#modify-durationUnit').val("day");
         $('#modify-durationCnt').val(data.duration);
         //duration;
@@ -43,8 +44,10 @@ function changePackage(id) {
         }
         $('#modify-iconUrl').val(data.icon);
         $('#modify-detailDescriptionIcon').val(data.detailDescriptionIcon);
+        $('#modify-smallIcon').val(data.smallIcon);
         $('#icon-img-index').attr('src', data.icon);
         $('#detail-img-index').attr('src', data.detailDescriptionIcon);
+        $('#small-icon-img-index').attr('src', data.smallIcon);
         var categoryId = data.healthCategory.id;
         var groupTypeUrl= commonUrl +'discovery/cms/listCategory/groupinfo';
         $.get(groupTypeUrl, function (groupTypes) {
@@ -77,17 +80,18 @@ function searchPackage(typeId){
 
     localStorage.setItem('typeId', typeId);
     var packagesUrl = commonUrl + "hcPackage/findPackagesByCategoryId/" + typeId;
-    console.log('packagesUrl : '+packagesUrl);
+    if(typeId == null || typeId.length == 0){
+        return ;
+    }
 
     $("#allDatas").html("");
     $.get(packagesUrl, function (data) {
         var datas = '';
-        console.log(data.ret_values);
+        //console.log(data.ret_values);
         $.each(data.ret_values, function (n, value) {
-            console.log(value);
+            //console.log(value);
             var categoryUrl = onePackageUrl + value.id;
             var categoryTitle = '';
-            console.log('categoryUrl : '+categoryUrl);
             $.ajax({
                 type : "get",
                 url : categoryUrl,
@@ -105,9 +109,9 @@ function searchPackage(typeId){
                 "<td><a href='#'>" + value.title + "</a></td>" +
                 "<td class='am-hide-sm-only'>" + value.description + "</td>" +
                 "<td class='am-hide-sm-only'>" + value.duration + "</td>" +
-                "<td class='am-hide-sm-only'>" + (value.recommend == 'true' ? '是':'否' )+ "</td>" +
-                "<td class='am-hide-sm-only'>" + (value.loops == '1' ? '是':'否' )+ "</td>" +
-                "<td class='am-hide-sm-only'>" + (value.needSupplemented == 'true' ? '是':'否' )+ "</td>" +
+                "<td class='am-hide-sm-only'>" + (value.recommend ? '是':'否' )+ "</td>" +
+                "<td class='am-hide-sm-only'>" + (value.loops ? '是':'否' )+ "</td>" +
+                "<td class='am-hide-sm-only'>" + (value.needSupplemented ? '是':'否' )+ "</td>" +
                 "<td><div class='am-btn-toolbar'><div class='am-btn-group am-btn-group-xs'>" +
                 "<button class='am-btn am-btn-default am-btn-xs am-text-secondary' type='button' onclick='changePackage(" + value.id + ")'>" +
                 "<span class='am-icon-pencil-square-o'></span> 编辑</button>" ;
@@ -130,20 +134,27 @@ function openImgUpload() {
     $('#change-modal').modal('toggle');
 }
 
-function modifyPackage(id){
+function modifyPackage(){
     var healthCategoryId = $('#type').val();
+    var id = $('#change-id').val();
+    if(id == null || id.length == 0){
+        alert("请选择需要修改的包");
+        return;
+    }
     console.log(healthCategoryId);
     var durationUnit = $('#modify-durationUnit').val(),
         durationCnt = $('#modify-durationCnt').val(),
         duration,
         title = $('#modify-title').val(),
         description = $('#modify-description').val(),
+        detailDescription = $('#modify-detailDescription').val(),
         recommend = $('#modify-recommend').val(),
         recommendValue = $('#modify-recommendValue').val(),
         isNeedSupplemented = $('#modify-isNeedSupplemented').val(),
         loops = $('#modify-loops').val(),
         iconUrl = $('#modify-iconUrl').val(),
-        detailDescriptionIcon = $('#modify-detailDescriptionIcon').val();
+        detailDescriptionIcon = $('#modify-detailDescriptionIcon').val(),
+        smallIcon = $('#modify-smallIcon').val();
     if(durationUnit == 'week'){
         duration = durationCnt * 7;
     } else if (durationUnit == "day") {
@@ -173,27 +184,30 @@ function modifyPackage(id){
         isNeedSupplemented = false;
     }
     if($('#modify-loops').is(':checked')){
-        loops = "1";
+        loops = true;
     } else {
-        loops = "0";
+        loops = false;
     }
 
     //校验字段
     var json ={
         "title": title,
         "description": description,
+        "detailDescription": detailDescription,
         "recommend": recommend,
         "recommendValue": recommendValue,
         "isNeedSupplemented": isNeedSupplemented,
         "loops": loops,
         "icon": iconUrl,
         "detailDescriptionIcon": detailDescriptionIcon,
-        "duration": duration
+        "duration": duration,
+        "smallIcon": smallIcon
     };
     json = JSON.stringify(json);
 
-    var updateUrl = updatePackageUrl + "/" + id;
+    var updateUrl = updatePackageUrl + id;
     console.log(json);
+    debugger;
     $.ajax({
         url: updateUrl,
         type: "POST",
@@ -204,7 +218,6 @@ function modifyPackage(id){
             if (response.ret_code == 0) {
                 alert('提交成功');
                 location.reload();
-                //clearHtml();
             }
             else
                 alert(response.err_msg);
@@ -215,27 +228,6 @@ function modifyPackage(id){
 
 
     });
-}
-
-function clearHtml(){
-    $('#durationCnt').val("");
-    $('#duration').val("");
-    $('#title').val("");
-    $('#description').val("");
-    $('#detailDescription').val("");
-    $('#recommend').val("");
-    $('#recommend').removeAttr("checked");
-    $('#recommend').removeAttr("value");
-    $('#recommendValue').val("");
-    $('#isNeedSupplemented').val("");
-    $('#isNeedSupplemented').removeAttr("checked");
-    $('#isNeedSupplemented').removeAttr("value");
-    $('#loops').val("");
-    $('#loops').removeAttr("checked");
-    $('#loops').removeAttr("value");
-    $('#iconUrl').val("");
-    $('#detailDescriptionIcon').val("");
-    $('#detail-img').attr('src', '');
 }
 
 
