c03d3a05ec43d587fbea74db12594b58c384a6db
wukai <wukai@wondersgroup.com>
2015-07-13 09:52:36 +0800

    用户发起提问时，新建一个订单记录。记录用户与医生的一次沟通记录

---
+++ src/main/java/com/wonders/xlab/healthcloud/entity/QuestionOrder.java
@@ -0,0 +1,48 @@
+package com.wonders.xlab.healthcloud.entity;
+
+import com.wonders.xlab.healthcloud.entity.customer.User;
+import com.wonders.xlab.healthcloud.entity.doctor.Doctor;
+
+import javax.persistence.*;
+
+/**
+ * 用户发起的问题订单
+ * Created by wukai on 15/7/12.
+ */
+@Entity
+@Table(name = "HC_QUESTION_ORDERS")
+public class QuestionOrder extends AbstractBaseEntity<Long> {
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private EmMessages messages;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private Doctor doctor;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public EmMessages getMessages() {
+        return messages;
+    }
+
+    public void setMessages(EmMessages messages) {
+        this.messages = messages;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+}
