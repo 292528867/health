f9798b3dd2e9bfefe6d0d67629e8c2a9c3858326
万华秋 <303701750@qq.com>
2015-07-13 18:10:41 +0800

    上传测试文件

--- src/test/java/com/wonders/xlab/healthcloud/service/hcpackage/HcpackageServiceTest.java
+++ src/test/java/com/wonders/xlab/healthcloud/service/hcpackage/HcpackageServiceTest.java
@@ -1,12 +1,6 @@
 package com.wonders.xlab.healthcloud.service.hcpackage;
 
-import com.wonders.xlab.framework.Application;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.Assert;
 
 import java.util.Calendar;
@@ -15,21 +9,18 @@ import java.util.Calendar;
  * Created by Jeffrey on 15/7/9.
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
 public class HcpackageServiceTest {
 
-    @Autowired
-    private HcpackageService hcpackageService;
+    private HcpackageService hcpackageService = new HcpackageService();
 
     @Test
     public void testCalculateClickCount() throws Exception {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2015, 7, 6);
+//        calendar.set(2015, 7, 6);
 
-        System.out.println(Math.pow(1.02,10));
-        int clickCount = hcpackageService.calculateClickCount(5, calendar.getTime());
-        Assert.isTrue(clickCount == 7);
+        System.out.println(Math.pow(1.02, 10));
+        int clickCount = hcpackageService.calculateClickCount(0, 7, calendar.getTime());
+        System.out.println("clickCount = " + clickCount);
+//        Assert.isTrue(clickCount == );
     }
 }
