3197a8f3ea691e7c9438c357e0f7c29b5aa56cb5
万华秋 <303701750@qq.com>
2015-07-13 17:56:04 +0800

    关注数不对

--- src/main/java/com/wonders/xlab/healthcloud/dto/hcpackage/UserPackageOrderDto.java
+++ src/main/java/com/wonders/xlab/healthcloud/dto/hcpackage/UserPackageOrderDto.java
@@ -1,6 +1,10 @@
 package com.wonders.xlab.healthcloud.dto.hcpackage;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.wonders.xlab.healthcloud.entity.discovery.HealthCategory;
+import com.wonders.xlab.healthcloud.service.hcpackage.HcpackageService;
+
+import java.util.Date;
 
 /**
  * Created by mars on 15/7/8.
@@ -85,6 +89,12 @@ public class UserPackageOrderDto {
      */
     private String loops;
 
+    @JsonIgnore
+    private Date createdDate;
+
+    @JsonIgnore
+    private int coefficient;
+
     private HealthCategory healthCategory;
 
     /**
@@ -161,7 +171,7 @@ public class UserPackageOrderDto {
     }
 
     public int getClickAmount() {
-        return clickAmount;
+        return HcpackageService.calculateClickCount(coefficient, clickAmount, createdDate);
     }
 
     public void setClickAmount(int clickAmount) {
@@ -254,5 +264,13 @@ public class UserPackageOrderDto {
 
     public void setCountTask(Integer countTask) {
         this.countTask = countTask;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setCoefficient(int coefficient) {
+        this.coefficient = coefficient;
     }
 }
