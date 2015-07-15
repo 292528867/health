1c9c4bb12a6698f239d4fd82f7eb63e55e76e01b
mars <cosmo_mars@outlook.com>
2015-07-15 20:23:30 +0800

    修改banner添加修改

--- src/main/java/com/wonders/xlab/healthcloud/dto/banner/BannerDto.java
+++ src/main/java/com/wonders/xlab/healthcloud/dto/banner/BannerDto.java
@@ -3,6 +3,7 @@ package com.wonders.xlab.healthcloud.dto.banner;
 import com.wonders.xlab.healthcloud.entity.banner.Banner;
 import com.wonders.xlab.healthcloud.entity.banner.BannerTag;
 import com.wonders.xlab.healthcloud.entity.banner.BannerType;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import javax.validation.constraints.NotNull;
@@ -16,7 +17,7 @@ public class BannerDto {
     private Long id;
 
     /** Banner标签 0 计划 1 发现 2 管家 3 问诊 4 商城 5 个人中心 6 购药 */
-//    @NotNull(message = "标签不能为空")
+    @NotNull(message = "标签不能为空")
 //    @Pattern(regexp = "^0|1|2|3|4|5|6$", message = "标签必须为 0日程｜1发现｜2管家｜3问诊｜4商城｜5个人中心｜6购药")
     private String bannerTag;
 
@@ -61,14 +62,14 @@ public class BannerDto {
                 Boolean.valueOf(enabled),
                 Integer.valueOf(position)
         );
-        if (bannerTag != null) {
+        if (!StringUtils.equals(bannerTag, "none")) {
             banner.setBannerTag(BannerTag.values()[NumberUtils.toInt(bannerTag)]);
         }
         return banner;
     }
 
     public Banner updateBanner(Banner banner) {
-        if (bannerTag != null) {
+        if (!StringUtils.equals(bannerTag, "none")) {
             banner.setBannerTag(BannerTag.values()[NumberUtils.toInt(bannerTag)]);
         }
         banner.setBannerType(BannerType.values()[NumberUtils.toInt(bannerType)]);
