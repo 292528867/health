3ba741fe576adf4cea65fa44b6c293d38f1d421e
mars <cosmo_mars@outlook.com>
2015-07-15 15:43:42 +0800

    修改显示标语

--- src/main/java/com/wonders/xlab/healthcloud/controller/banner/ToplineController.java
+++ src/main/java/com/wonders/xlab/healthcloud/controller/banner/ToplineController.java
@@ -1,7 +1,103 @@
 package com.wonders.xlab.healthcloud.controller.banner;
 
+import com.wonders.xlab.framework.controller.AbstractBaseController;
+import com.wonders.xlab.framework.repository.MyRepository;
+import com.wonders.xlab.healthcloud.dto.banner.ToplineDto;
+import com.wonders.xlab.healthcloud.dto.result.ControllerResult;
+import com.wonders.xlab.healthcloud.entity.banner.Topline;
+import com.wonders.xlab.healthcloud.repository.banner.ToplineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 /**
  * Created by mars on 15/7/15.
  */
-public class ToplineController {
+@RestController
+@RequestMapping("topline")
+public class ToplineController extends AbstractBaseController<Topline, Long> {
+
+    @Autowired
+    private ToplineRepository toplineRepository;
+
+    @Override
+    protected MyRepository<Topline, Long> getRepository() {
+        return toplineRepository;
+    }
+
+    /**
+     * 头条列表
+     * @return
+     */
+    @RequestMapping(value = "listToplines", method = RequestMethod.GET)
+    public Object listToplines() {
+        Sort sort = new Sort(Sort.Direction.DESC, "lastModifiedDate");
+        return new ControllerResult<>()
+                .setRet_code(0)
+                .setRet_values(toplineRepository.findByEnabledTrue(sort))
+                .setMessage("成功");
+    }
+
+    /**
+     * 添加头条
+     * @param toplineDto
+     * @param result
+     * @return
+     */
+    @RequestMapping(value = "addTopline", method = RequestMethod.POST)
+    public Object addTopline(@RequestBody @Valid ToplineDto toplineDto, BindingResult result) {
+        if (result.hasErrors()) {
+            StringBuilder builder = new StringBuilder();
+            for (ObjectError error : result.getAllErrors()) {
+                builder.append(error.getDefaultMessage());
+            }
+            return new ControllerResult<>()
+                    .setRet_code(-1)
+                    .setRet_values(builder.toString())
+                    .setMessage("失败");
+        }
+        toplineRepository.save(toplineDto.toNewTopline());
+        return new ControllerResult<>()
+                .setRet_code(0)
+                .setRet_values("添加成功")
+                .setMessage("成功");
+    }
+
+    /**
+     * 修改头条
+     * @param toplineId
+     * @param toplineDto
+     * @param result
+     * @return
+     */
+    @RequestMapping(value = "updateTopline/{toplineId}", method = RequestMethod.POST)
+    public Object updateTopline(@PathVariable long toplineId,
+                                @RequestBody @Valid ToplineDto toplineDto, BindingResult result) {
+        if (result.hasErrors()) {
+            StringBuilder builder = new StringBuilder();
+            for (ObjectError error : result.getAllErrors()) {
+                builder.append(error.getDefaultMessage());
+            }
+            return new ControllerResult<>()
+                    .setRet_code(-1)
+                    .setRet_values(builder.toString())
+                    .setMessage("失败");
+        }
+        Topline topline = toplineRepository.findOne(toplineId);
+        if (topline == null) {
+            return new ControllerResult<>()
+                    .setRet_code(-1)
+                    .setRet_values("竟然没找到")
+                    .setMessage("失败");
+        }
+        toplineRepository.save(toplineDto.updateTopline(topline));
+        return new ControllerResult<>()
+                .setRet_code(0)
+                .setRet_values("添加成功")
+                .setMessage("成功");
+    }
 }
--- src/main/java/com/wonders/xlab/healthcloud/repository/banner/ToplineRepository.java
+++ src/main/java/com/wonders/xlab/healthcloud/repository/banner/ToplineRepository.java
@@ -1,7 +1,17 @@
 package com.wonders.xlab.healthcloud.repository.banner;
 
+import com.wonders.xlab.framework.repository.MyRepository;
+import com.wonders.xlab.healthcloud.entity.banner.Topline;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
 /**
  * Created by mars on 15/7/15.
  */
-public class ToplineRepository {
+public interface ToplineRepository extends MyRepository<Topline, Long> {
+
+    List<Topline> findByEnabledTrue(Sort sort);
+
+
 }
