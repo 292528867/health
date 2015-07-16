dac31e53ef9ebeeb20ac2b6041238adc2132f371
刘泽龙 <liuzelong@wondersgroup.com>
2015-07-16 12:32:05 +0800

    修复一下

--- src/main/webapp/console/js/indexEdit/firstSliderLoad.js
+++ src/main/webapp/console/js/indexEdit/firstSliderLoad.js
@@ -2,23 +2,21 @@
  * Created by wade on 15/5/25.
  */
 
-var packageUrl= commonUrl +'hcPackage/listHcPackage',
-    bannerListUrl= commonUrl +'homepage/listBannerForConsole',
-    topicListUrl= commonUrl +'homepage/retrieveHealthInfos/',
-    addBanner= commonUrl +'homepage/addBanner',
-    updateBannerUrl= commonUrl +'homepage/updateBanner/';
-
+var packageUrl = commonUrl + 'hcPackage/listHcPackage',
+    bannerListUrl = commonUrl + 'banner/listBannerForConsole', topicListUrl = commonUrl + 'banner/retrieveHealthInfos/',
+    addBanner = commonUrl + 'banner/addBanner',
+    updateBannerUrl = commonUrl + 'banner/updateBanner/';
 
 //编辑
 function changeType(e) {
     $("#package,#topic").closest(".am-form-group").addClass("am-hide");
-    $(".am-modal-bd input,.am-modal-bd select").each(function(){
-        if($(this).attr("id")=="package")
-             return true;
+    $(".am-modal-bd input,.am-modal-bd select").each(function () {
+        if ($(this).attr("id") == "package")
+            return true;
         $(this).val("");
     });
-    if(e){
-        var data= eval("("+$(e).closest("tr").attr("data-all")+")");
+    if (e) {
+        var data = eval("(" + $(e).closest("tr").attr("data-all") + ")");
         console.log(data);
         $("#id").val(data.id);
         $("#title").val(data.title);
@@ -36,13 +34,13 @@ function changeType(e) {
 /**
  * 获取所有健康包
  */
-function getPackageList(){
+function getPackageList() {
     $.get(packageUrl, function (groupTypes) {
         console.log(groupTypes);
         var groups = groupTypes.ret_values,
             typeList = '';
         $.each(groups, function (n, group) {
-            typeList+='<option value="'+group.id+'">'+group.title+'</option>';
+            typeList += '<option value="' + group.id + '">' + group.title + '</option>';
         });
         $("#package").empty().append(typeList);
     });
@@ -51,36 +49,36 @@ function getPackageList(){
 /**
  * 根据包  获取文章
  */
-function getTopicList(id){
-    $.get(topicListUrl+id, function (groupTypes) {
+function getTopicList(id) {
+    $.get(topicListUrl + id, function (groupTypes) {
         console.log(groupTypes);
         var groups = groupTypes.ret_values,
             typeList = '';
         $.each(groups, function (n, group) {
-            typeList+='<option value="'+group.id+'">'+group.title+'</option>';
+            typeList += '<option value="' + group.id + '">' + group.title + '</option>';
         });
         $("#topic").empty().append(typeList);
     });
 }
 
-$("#bannerTag").on("change",function(){
+$("#bannerTag").on("change", function () {
     //如果是计划或者发现的 加载package
-    if($(this).val()==0 || $(this).val()==1){
+    if ($(this).val() == 0 || $(this).val() == 1) {
         $("#package,#topic").closest(".am-form-group").removeClass("am-hide");
-    }else{
+    } else {
         $("#package,#topic").closest(".am-form-group").addClass("am-hide");
         $("#topic").html("<option></option>");
         $("#articleId").val("");
     }
 });
 
-$("#package").on("change",function(){
+$("#package").on("change", function () {
     $("#topic").html("<option></option>");
     getTopicList($(this).val());
 });
 
 
-$("#topic").on("change",function(){
+$("#topic").on("change", function () {
     $("#articleId").val($(this).val());
 });
 
@@ -93,38 +91,38 @@ $.post(bannerListUrl, function (groupTypes) {
     console.log(groupTypes);
     var groups = groupTypes.ret_values,
         typeList = '';
-    var text="",open="";
+    var text = "", open = "";
     $.each(groups, function (n, group) {
 
-        if(group.bannerType=="1"){
-            text="二级";
-        }else if (group.bannerType=="0"){
-            text="一级";
+        if (group.bannerType == "1") {
+            text = "二级";
+        } else if (group.bannerType == "0") {
+            text = "一级";
         }
-        if(group.enabled=="true"){
-            open="禁用";
-        }else if(group.enabled=="false"){
-            open="启用";
+        if (group.enabled == "true") {
+            open = "禁用";
+        } else if (group.enabled == "false") {
+            open = "启用";
         }
-        typeList+='<tr data-all=\''+JSON.stringify(group)+'\'>'+
-        '<td>'+group.id+'</td>'+
-        '<td><a href="javascript:void(0)" onclick="enabledBanner(this)">'+ open +'</a></td>'+
-        '<td>'+ text +'</td>'+
-        '<td class="am-hide-sm-only">'+ group.position +'</td>'+
-        '<td class="am-hide-sm-only">'+ group.title +'</td>'+
-        '<td class="am-hide-sm-only"><img src="'+ group.picUrl +'" /></td>'+
-        '<td class="am-hide-sm-only">'+ group.articleId + "."+ group.articleTitle +'</td>'+
-        '<td class="am-hide-sm-only">'+ group.linkUrl +'</td>'+
-        '<td>'+
-        '<div class="am-btn-toolbar">'+
-        '<div class="am-btn-group am-btn-group-xs">'+
-        '<button class="am-btn am-btn-default am-btn-xs am-text-secondary" type="button"'+
-        'onclick="changeType(this)"><span class="am-icon-pencil-square-o"></span> 编辑'+
-        '</button>'+
-        '</div>'+
-        '</div>'+
-        '</td>'+
-        '</tr>';
+        typeList += '<tr data-all=\'' + JSON.stringify(group) + '\'>' +
+            '<td>' + group.id + '</td>' +
+            '<td><a href="javascript:void(0)" onclick="enabledBanner(this)">' + open + '</a></td>' +
+            '<td>' + text + '</td>' +
+            '<td class="am-hide-sm-only">' + group.position + '</td>' +
+            '<td class="am-hide-sm-only">' + group.title + '</td>' +
+            '<td class="am-hide-sm-only"><img src="' + group.picUrl + '" /></td>' +
+            '<td class="am-hide-sm-only">' + group.articleId + "." + group.articleTitle + '</td>' +
+            '<td class="am-hide-sm-only">' + group.linkUrl + '</td>' +
+            '<td>' +
+            '<div class="am-btn-toolbar">' +
+            '<div class="am-btn-group am-btn-group-xs">' +
+            '<button class="am-btn am-btn-default am-btn-xs am-text-secondary" type="button"' +
+            'onclick="changeType(this)"><span class="am-icon-pencil-square-o"></span> 编辑' +
+            '</button>' +
+            '</div>' +
+            '</div>' +
+            '</td>' +
+            '</tr>';
     });
     $('#allDatas').empty().append(typeList);
 });
@@ -133,57 +131,57 @@ $.post(bannerListUrl, function (groupTypes) {
 /**
  * 提交
  */
-function updateBanner(){
-    var title=$("#title").val();
-    var picUrl=$("#picUrl").val();
-    var linkUrl=$("#linkUrl").val();
-    var position=$("#position").val();
-    var bannerTag=$("#bannerTag").val();
-    var bannerType=$("#bannerType").val();
-    var articleId=$("#articleId").val();
+function updateBanner() {
+    var title = $("#title").val();
+    var picUrl = $("#picUrl").val();
+    var linkUrl = $("#linkUrl").val();
+    var position = $("#position").val();
+    var bannerTag = $("#bannerTag").val();
+    var bannerType = $("#bannerType").val();
+    var articleId = $("#articleId").val();
 
-    if(picUrl==""){
+    if (picUrl == "") {
         alert("请先上传图片");
     }
-    var json ={
+    var json = {
         "title": title,
         "picUrl": picUrl,
-        "linkUrl":linkUrl,
-        "bannerTag":bannerTag,
-        "bannerType":bannerType,
-        "articleId":articleId,
-        "position":position,
-        "enabled":true
+        "linkUrl": linkUrl,
+        "bannerTag": bannerTag,
+        "bannerType": bannerType,
+        "articleId": articleId,
+        "position": position,
+        "enabled": true
     };
 
 
-    if($("#id").val()){
+    if ($("#id").val()) {
         json.id = $("#id").val();
     }
     postBanner(json);
 
 }
 
-function enabledBanner(e){
-    var param =eval("("+$(e).closest("tr").attr("data-all")+")");
-    var json ={
+function enabledBanner(e) {
+    var param = eval("(" + $(e).closest("tr").attr("data-all") + ")");
+    var json = {
         "title": param.title,
         "picUrl": param.picUrl,
-        "linkUrl":param.linkUrl,
-        "articleId":param.articleId,
-        "bannerTag":param.bannerTag,
-        "bannerType":param.bannerType,
-        "position":param.position,
-        "enabled":param.enabled=="true"?"false":"true",
-        "id":param.id
+        "linkUrl": param.linkUrl,
+        "articleId": param.articleId,
+        "bannerTag": param.bannerTag,
+        "bannerType": param.bannerType,
+        "position": param.position,
+        "enabled": param.enabled == "true" ? "false" : "true",
+        "id": param.id
     };
     postBanner(json);
 }
 
-function postBanner(json){
+function postBanner(json) {
     var ajaxUrl = addBanner;
-    if(json.id){
-        ajaxUrl=updateBannerUrl+json.id;
+    if (json.id) {
+        ajaxUrl = updateBannerUrl + json.id;
         delete json.id;
     }
 
