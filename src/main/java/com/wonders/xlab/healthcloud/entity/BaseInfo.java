3c10c1ddee7501a38009eec046aa3a4b4dcc2835
万华秋 <303701750@qq.com>
2015-07-16 10:52:18 +0800

    修改性别枚举返回

--- src/main/java/com/wonders/xlab/healthcloud/entity/BaseInfo.java
+++ src/main/java/com/wonders/xlab/healthcloud/entity/BaseInfo.java
@@ -133,8 +133,8 @@ public abstract class BaseInfo<ID extends Serializable> extends AbstractPersista
         this.iconUrl = iconUrl;
     }
 
-    public int getSex() {
-        return sex.ordinal();
+    public Sex getSex() {
+        return sex;
     }
 
     public void setSex(Sex sex) {
