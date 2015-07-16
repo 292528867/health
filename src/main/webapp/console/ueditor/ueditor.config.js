9640a687b5b1d0553dad6355837825562ffcc36e
刘泽龙 <liuzelong@wondersgroup.com>
2015-07-16 12:37:56 +0800

    ueditor

--- src/main/webapp/console/ueditor/ueditor.config.js
+++ src/main/webapp/console/ueditor/ueditor.config.js
@@ -31,8 +31,8 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: 'http://10.10.11.17:8080/ue'
-        //, serverUrl: 'http://101.231.124.8:45677/xlab-healthcloud/ue'
+        //, serverUrl: 'http://10.10.11.17:8080/ue'
+        , serverUrl: 'http://101.231.124.8:45677/xlab-healthcloud/ue'
 
         ////工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         //,imageUrl:"http://101.231.124.8:45677/xlab-healthcloud/discovery/cms/uploadPicture"             //图片上传提交地址
